c723add30884e7f73107b212b0ccbaa7185dbcca
Susheel Varma <susheel.varma@gmail.com>
2013-07-03 16:53:58 +0100

    Taverna Install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -d /var/lib/tomcat6/webapps/taverna-server ]
+then
+  echo "You already have Taverna Server installed. You'll need to remove Taverna Server if you want to install"
+  exit
+fi
+
+sudo apt-get update             # Update package cache
+sudo apt-get -y install git     # Install git
+
+# Clone taverna-server repository
+git clone https://github.com/susheel/taverna-server.git
+cd taverna-server
+
+./bootstrap.sh &>~/taverna-install.log
