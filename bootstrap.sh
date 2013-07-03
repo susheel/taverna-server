995aad1fac923ce90a0db7b8d4d82ff034361840
Susheel Varma <susheel.varma@gmail.com>
2013-07-03 16:13:09 +0100

    Bootstrap Java (OpenJDK JRE - Headless)

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,8 @@ echo -e "\n\n#### 0. System Packages\n\n"
 sudo apt-get update
 
 
+echo -e "\n\n#### 1. Java (OpenJDK-7)\n\n"
+sudo apt-get -y install openjdk-7-jre-headless
+FIND_JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+export JAVA_HOME=${FIND_JAVA_HOME%/}
+sudo echo "export JAVA_HOME=$JAVA_HOME" >> .bashrc
