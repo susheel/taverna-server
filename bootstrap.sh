e398fe7b452932fb257c754e7209422418d22cdd
Susheel Varma <susheel.varma@gmail.com>
2013-07-03 16:15:09 +0100

    Install Java Cryptography Extensions

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 echo -e "\n\n#### 0. System Packages\n\n"
 sudo apt-get update
+sudo apt-get -y install unzip
 
 
 echo -e "\n\n#### 1. Java (OpenJDK-7)\n\n"
@@ -9,3 +10,9 @@ sudo apt-get -y install openjdk-7-jre-headless
 FIND_JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 export JAVA_HOME=${FIND_JAVA_HOME%/}
 sudo echo "export JAVA_HOME=$JAVA_HOME" >> .bashrc
+
+
+echo -e "\n\n#### 2. Java Cryptography Extensions\n\n"
+unzip config/UnlimitedJCEPolicyJDK7.zip
+sudo cp -f UnlimitedJCEPolicy/*.jar $JAVA_HOME/lib/security/
+rm -rf UnlimitedJCEPolicy
